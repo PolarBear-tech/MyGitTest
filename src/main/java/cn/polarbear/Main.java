5c437413a3b77d1a91dee167b1bf326d40ceea22
= <=>
2025-01-02 14:01:08 +0800

    Initial commit

---
+++ src/main/java/cn/polarbear/Main.java
@@ -0,0 +1,12 @@
+package cn.polarbear;
+
+/**
+ * @author PolarBear
+ * @date 24.12.29
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, Git!");
+    }
+}

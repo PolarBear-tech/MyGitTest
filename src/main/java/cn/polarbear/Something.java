a1d8d6a2e7bc31400648126a5132d74e05a2dca6
PolarBear <L17639011528@163.com>
2025-01-03 10:15:08 +0800

    create Something.java

---
+++ src/main/java/cn/polarbear/Something.java
@@ -0,0 +1,15 @@
+package cn.polarbear;
+
+/**
+ * @author PolarBear
+ * @date 25.1.3
+ */
+public class Something {
+    public static void main(String[] args) {
+        doSomething();
+    }
+
+    private static void doSomething() {
+        System.out.println("Do something");
+    }
+}

afded921f3d27347a28f4141f250d16cd4a599b6
PolarBear <L17639011528@163.com>
2025-01-03 10:37:58 +0800

    Add Cat.java

---
+++ src/main/java/cn/polarbear/Cat.java
@@ -0,0 +1,11 @@
+package cn.polarbear;
+
+/**
+ * @author PolarBear
+ * @date 25.1.3
+ */
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("Here is cat!");
+    }
+}
